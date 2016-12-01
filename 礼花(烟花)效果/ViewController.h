4ce354fa10915e885a755866adda41981046efe7
残月 <632342527@qq.com>
2016-12-01 16:26:46 +0800

    Initial Commit

---
+++ 礼花(烟花)效果/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  礼花(烟花)效果
+//
+//  Created by 王奥东 on 16/12/1.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 礼花(烟花)效果/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  礼花(烟花)效果
+//
+//  Created by 王奥东 on 16/12/1.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
